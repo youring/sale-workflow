65e3715563da071ed1cb9523257539019a4590c3
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2018-07-31 11:25:23 +0200

    [11.0][ADD] sale_general_discount: New module

---
+++ sale_general_discount/models/sale.py
@@ -0,0 +1,79 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    discount = fields.Float(
+        digits=dp.get_precision('Discount'),
+        default=0.0,
+        string='Discount (%)',
+    )
+
+    @api.multi
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        for order in self:
+            super(SaleOrder, self).onchange_partner_id()
+            if order.partner_id.discount:
+                order.discount = order.partner_id.discount
+
+    @api.multi
+    @api.onchange('discount')
+    def onchange_discount(self):
+        for order in self:
+            if order.partner_id.discount:
+                order.discount = order.partner_id.discount
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    amount_order_discount = fields.Monetary(
+        compute='_compute_amount',
+        string='Discount',
+        readonly=True,
+        store=True,
+    )
+
+    @api.depends('order_id.discount')
+    def _compute_amount(self):
+        for line in self:
+            line_discount = line.discount
+            line_price_unit = line.price_unit
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            amount_discount = price * line.order_id.discount / 100
+            price_wo_discount = price - amount_discount
+            line.update({
+                'price_unit': price_wo_discount,
+                'amount_order_discount': amount_discount,
+                'discount': 0.0,
+            })
+            super(SaleOrderLine, self)._compute_amount()
+            line.update({
+                'price_unit': line_price_unit,
+                'discount': line_discount,
+            })
+
+    @api.depends('order_id.discount')
+    def _get_price_reduce(self):
+        for line in self:
+            super(SaleOrderLine, self)._get_price_reduce()
+            if line.order_id.discount:
+                line.price_reduce *= (1 - line.order_id.discount / 100)
+
+    @api.multi
+    def _prepare_invoice_line(self, qty):
+        vals = super(SaleOrderLine, self)._prepare_invoice_line(qty)
+        if self.discount and self.order_id.discount:
+            amount_weight = self.price_unit * self.product_uom_qty
+            discount = (
+               amount_weight - self.price_subtotal) * 100 / amount_weight
+        else:
+            discount = self.discount or self.order_id.discount
+        precision = self.env['decimal.precision'].precision_get('Discount')
+        vals['discount'] = round(discount, precision)
+        return vals
